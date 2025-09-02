bd6450967335a41efb63112b4edc8267693e4c93
고진영 <jim00011128@gmail.com>
2025-09-02 23:00:32 +0900

    feat: ControllerAdvice 클래스 추가
    - 테스트코드 추가

--- src/test/java/tobyspring/splearn/adapter/webapi/MemberApiTest.java
+++ src/test/java/tobyspring/splearn/adapter/webapi/MemberApiTest.java
@@ -1,16 +1,20 @@
 package tobyspring.splearn.adapter.webapi;
 
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.assertj.MockMvcTester;
 import org.springframework.test.web.servlet.assertj.MvcTestResult;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.transaction.annotation.Transactional;
 import tobyspring.splearn.adapter.webapi.dto.MemberRegisterResponse;
+import tobyspring.splearn.application.member.provided.MemberRegister;
 import tobyspring.splearn.application.member.required.MemberRepository;
 import tobyspring.splearn.domain.MemberStatus;
 import tobyspring.splearn.domain.member.Member;
@@ -20,6 +24,7 @@ import tobyspring.splearn.domain.member.MemberRegisterRequest;
 import java.io.IOException;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static tobyspring.splearn.AssertThatUtils.equalsTo;
 import static tobyspring.splearn.AssertThatUtils.notNull;
 
@@ -42,6 +47,7 @@ public class MemberApiTest {
     final MockMvcTester mvcTester;
     final ObjectMapper objectMapper;
     final MemberRepository memberRepository;
+    final MemberRegister memberRegister;
 
 
     @Test
@@ -66,5 +72,21 @@ public class MemberApiTest {
         assertThat(member.getEmail().address()).isEqualTo(request.email());
         assertThat(member.getNickname()).isEqualTo(request.nickname());
         assertThat(member.getStatus()).isEqualTo(MemberStatus.PENDING);
+    }
+    
+    @Test
+    void duplicateEmail() throws JsonProcessingException {
+        memberRegister.register(MemberFixture.createMemberRegisterRequest());
+
+        MemberRegisterRequest request = MemberFixture.createMemberRegisterRequest();
+        String requestJson = objectMapper.writeValueAsString(request);
+
+        MvcTestResult result = mvcTester.post().uri("/api/members")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(requestJson).exchange();
+
+        assertThat(result)
+                .apply(print())
+                .hasStatus(HttpStatus.CONFLICT);
     }
 }
